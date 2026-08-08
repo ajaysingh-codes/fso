b6b70d843b64315517823217c3acfaa83fb67e56
Ajay Singh <ajay.singh@sjsu.edu>
2026-08-08 21:16:34 +0200

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -7,6 +7,8 @@ app.use(express.json())
 
 app.use(cors())
 
+app.use(express.static('dist'))
+
 morgan.token('post-log', function (req, res) { 
     if (req.method === 'POST') {
         return JSON.stringify(req.body)
