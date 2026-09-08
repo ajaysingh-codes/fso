ae4b490fe584db471217fc942fc7868eece88735
Ajay Singh <ajay.singh@sjsu.edu>
2026-09-08 11:18:35 +0200

    refactor(4.2): split bloglist into app, controllers, models, and config

---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,11 @@
+// model for blog collection
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+  })
+
+module.exports = mongoose.model('Blog', blogSchema)
